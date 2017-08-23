49a1c800e68eb9b86fa6f19152db6ea94022aec6
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2017-08-23 16:53:21 -0500

    working program

--- main.py
+++ main.py
@@ -1,8 +1,56 @@
 import requests
-from bs4 import BeautifulSoup
+import bs4
+import time
+import urllib
 
-response = requests.get('https://en.wikipedia.org/wiki/Dead_Parrot_sketch')
-html = response.text
+start_url = "https://en.wikipedia.org/wiki/Special:Random"
+target_url = "https://en.wikipedia.org/wiki/Philosophy"
 
-soup = BeautifulSoup(html, 'html.parser')
-print(soup.title)
+def find_first_link(url):
+    response = requests.get(url)
+    html = response.text
+    soup = bs4.BeautifulSoup(html, "html.parser")
+
+    content_div = soup.find(id="mw-content-text").find(class_="mw-parser-output")
+
+    article_link = None
+
+    for element in content_div.find_all("p", recursive=False):
+        if element.find("a", recursive=False):
+            article_link = element.find("a", recursive=False).get('href')
+            break
+
+    if not article_link:
+        return
+
+    first_link = urllib.parse.urljoin('https://en.wikipedia.org/', article_link)
+
+    return first_link
+
+def continue_crawl(search_history, target_url, max_steps=25):
+    if search_history[-1] == target_url:
+        print('Target article found! ' + search_history[-1])
+        return False
+    elif len(search_history) >= max_steps:
+        print('We have reached max steps, aborting search.')
+        return False
+    elif search_history[-1] in search_history[:-1]:
+        print('Already found ' + search_history[-1] + ', aborting search.')
+        return False
+    else:
+        return True
+
+article_chain = [start_url]
+
+while continue_crawl(article_chain, target_url):
+    print(article_chain[-1])
+
+    first_link = find_first_link(article_chain[-1])
+
+    if not first_link:
+        print("There are no links in the current article, exiting search")
+        break
+
+    article_chain.append(first_link)
+
+    time.sleep(2)
