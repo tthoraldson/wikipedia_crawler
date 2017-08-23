8972edca2bf3bb33380c336d72982f4e138f97e5
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2017-08-23 15:19:32 -0500

    continue crawl

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 response = requests.get('https://en.wikipedia.org/wiki/Dead_Parrot_sketch')
-# print(response.text)
-# print(type(response.text))
+html = response.text
 
-soup = BeautifulSoup(response, 'html.parser')
-soup.title
+soup = BeautifulSoup(html, 'html.parser')
+print(soup.title)
